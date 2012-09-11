98b14710dc3debbf927b12ab0af2631251cda0a9
ChunAllen <achun@appsource.biz>
2012-09-11 15:29:25 +0800

    add and view user

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.8'
 gem 'pg'
 gem 'devise'
 gem 'twitter-bootstrap-rails'
+gem 'simple_form'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 AuthLogic::Application.routes.draw do
   devise_for :users
 
+  resources :users
+
   root :to => 'home#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
