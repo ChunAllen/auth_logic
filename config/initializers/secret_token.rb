6055ae6a7276a8fd22a7c4d0917a9926fae0390f
ChunAllen <achun@appsource.biz>
2012-09-11 14:36:03 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthLogic::Application.config.secret_token = 'c673ce33b73a4823fdfe97e7daba095e3a079db55371a776b3562eb219eb3ab140d9507f0056c2607ec19afef6dbdf406bc019a712b22dc6343eccc8f0214052'
